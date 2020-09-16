d1461e31b941fea3fabee7909029d07ecaf7104b
AA666666 <52806201+AA666666@users.noreply.github.com>
2020-09-16 00:59:05 -0700

    2

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 q=int(input('type 1 if you want to do multiplication,type 2 if you want to do division:'))
+n1=float(input('please type your fist number:'))
+n2=float(input('please type your second number:'))
+n3=float(input('please type your third number:'))
+
+def add(n1,n2,n3):
+ n4=n1+n2+n3
+ return n4
+
