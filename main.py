f858950a7512bc20fa21dd2fc79338f5e96168e9
AA666666 <52806201+AA666666@users.noreply.github.com>
2020-09-16 01:02:31 -0700

    2

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ n1=float(input('please type your fist number:'))
 n2=float(input('please type your second number:'))
 n3=float(input('please type your third number:'))
 
-def add(n1,n2,n3):
- n4=n1+n2+n3
+def mul(n1,n2,n3):
+ n4=n1*n2*n3
  return n4
+
 
